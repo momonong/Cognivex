f79be259cb3e612e1580969153812c6b0ebab159
Morris <morrischen2.9@gmail.com>
2025-06-11 16:31:44 +0800

    Try deifferent activation overlapping method

--- scripts/capsnet/check_map.py
+++ scripts/capsnet/check_map.py
@@ -7,9 +7,9 @@ activation_img = nib.load("output/capsnet/sub-14_conv3_resampled_to_atlas.nii.gz
 # 顯示 activation map
 display = plotting.plot_stat_map(
     activation_img,
-    display_mode="ortho",
+    display_mode="mosaic",
     threshold=0.1,
     title="Activation Map (sub-14 conv3)"
 )
-display.savefig("figures/capsnet/activation_map.png")
-print("✅ Activation map displayed and saved as figures/capsnet/activation_map.png")
+display.savefig("figures/capsnet/activation_map_mosaic.png")
+print("✅ Activation map displayed and saved as figures/capsnet/activation_map_mosaic.png")
